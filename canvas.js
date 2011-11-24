124a438fb707f51e5dc8eda683c7afe60c92c450
Alexander Mihalicyn <alexander@mihalicyn.ru>
2011-11-24 20:12:06 +0600

    done

--- canvas.js
+++ canvas.js
@@ -14,11 +14,26 @@ Canvas.prototype = {
 		} else {
 			throw new Error('WTF!?');
 		}
+		
+		this.ctx.fillStyle = "red";
+		this.ctx.strokeStyle = "black";
 	},
 
 	circle: function( x0, y0, z0, r ) {
 		this.ctx.beginPath();
-		this.ctx.arc( x0, y0, r, 0, 2*Math.PI, false );
+		this.ctx.arc( x0, (this.canvas.height - y0), r, 0, 2*Math.PI, false );
 		this.ctx.fill();
+	},
+
+	plane: function( A, B, C, D ) {
+		this.ctx.beginPath();
+		this.ctx.moveTo( 0, (this.canvas.height - D/B) );
+		this.ctx.lineTo( this.canvas.width, this.canvas.height - ( D - A*this.canvas.width )/B );
+		this.ctx.closePath();
+		this.ctx.stroke();
+	},
+
+	clear: function() {
+		this.ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height );
 	}
 }
